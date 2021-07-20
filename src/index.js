af90bedfbc24a447b64c799c7f263ba7b0e88807
ochom <ochomrichard752@gmail.com>
2021-07-20 19:26:43 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Sidenav, HomeContent } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sidenav />
+    <HomeContent />
   </React.StrictMode>,
   document.getElementById('root')
 );
