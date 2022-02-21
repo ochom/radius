6fa62d69c668879647425c47d0a99e8fa769cea9
ochom <ochomrichard752@gmail.com>
2022-02-21 10:11:07 +0300

    reports feature comming soon

--- src/Components/library/reports.jsx
+++ src/Components/library/reports.jsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { Service } from '../../API/service';
 import { PageConstruction } from '../customs/empty-page';
-import { DataTable } from '../customs/table';
 
 
 export default function Reports() {
