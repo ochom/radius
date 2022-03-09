87687a5605759da3e42d43b8e270c914c2a31e1c
ochom <ochomrichard752@gmail.com>
2022-03-09 13:38:56 +0300

    update submenu css

--- src/Components/customs/index.jsx
+++ src/Components/customs/index.jsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from "react-router-dom";
 import { logout } from '../../reducers/auth-reducer';
-import profile from "../../static/profile.png";
 import Footer from './footers';
 
 
@@ -89,7 +88,7 @@ function TopBar(props) {
             </Badge>
             <Tooltip title="Open settings" arrow>
               <IconButton onClick={handleOpen}>
-                <Avatar alt={user.firstName} src={profile} />
+                <Avatar src={user.logo} />
               </IconButton>
             </Tooltip>
             <Menu
